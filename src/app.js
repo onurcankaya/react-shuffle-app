9d36633eefd8a1dd8018aaa10a26bfaf870a8be5
onurcankaya <onur.eren.cankaya@gmail.com>
2018-07-22 19:23:05 +0200

    implemented the necessary components for the app

--- src/app.js
+++ src/app.js
@@ -1,57 +1,74 @@
-console.log('app.js is running')
+class App extends React.PureComponent {
+  render() {
+    const title = 'Shuffle'
+    const subtitle = 'Put your choice in the hands of a computer'
+    const options = [1, 3, 5]
 
-const app = {
-    title: "Shuffle App",
-    subtitle: "JSX is running",
-    options: []
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault()
-
-    const option = e.target.elements.option.value
-
-    if (option) {
-        app.options.push(option)
-        e.target.elements.option.value = ''
-        render()
-    }
-}
-
-const onShuffle = () => {
-    const randomNumber = Math.floor(Math.random() * app.options.length)
-    const selectedOption = app.options[randomNumber]
-
-    if(selectedOption) {
-        alert(selectedOption)
-    }
-}
-
-const onRemoveAll = () => {
-    app.options = []
-    render()
-}
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
-            <button disabled={app.options.length === 0} onClick={onShuffle}>Make your pick</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                {app.options.map((option) => <li key={option}>{option}</li>)}
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>Add Option</button>
-            </form>
-        </div>
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options} />
+        <AddOption />
+      </div>
     )
-    
-    ReactDOM.render(template, document.getElementById('root'))
+  }
 }
 
-render()
+class Header extends React.PureComponent {
+  render() {
+    const { title, subtitle } = this.props
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </div>
+    )
+  }
+}
+
+class Action extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <button>Shuffle</button>
+      </div>
+    )
+  }
+}
+
+class Options extends React.PureComponent {
+  render() {
+    const { options } = this.props
+
+    return (
+      <ol>
+        {options.map((option) => {
+          return <Option option={option} key={option} />
+        })}
+      </ol>
+    )
+  }
+}
+
+class Option extends React.PureComponent {
+  render() {
+    const { option } = this.props
+
+    return <div>{option}</div>
+  }
+}
+
+class AddOption extends React.PureComponent {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <button>Add Option</button>
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
