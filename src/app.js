a87b332f03bbb93d6d4def80ab0f691072405623
onurcankaya <onur.eren.cankaya@gmail.com>
2018-07-23 19:45:44 +0200

    implemented addOption method to the form submit event handler

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
 class App extends React.PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      options: [1, 3, 5],
+    }
+
+    this.handleAddOption = this.handleAddOption.bind(this)
+  }
+
+  handleAddOption(e) {
+    e.preventDefault()
+
+    let option = e.target.elements.option.value.trim()
+
+    if (option) {
+      this.setState({ options: this.state.options.concat([option]) })
+      e.target.elements.option.value = ''
+      console.log(this.state.options)
+    }
+  }
+
   render() {
     const title = 'Shuffle'
     const subtitle = 'Put your choice in the hands of a computer'
-    const options = [1, 3, 5]
+    const { options } = this.state
 
     return (
       <div>
         <Header title={title} subtitle={subtitle} />
         <Action />
         <Options options={options} />
-        <AddOption />
+        <AddOption handleAddOption={this.handleAddOption} />
       </div>
     )
   }
@@ -29,25 +51,34 @@ class Header extends React.PureComponent {
 }
 
 class Action extends React.PureComponent {
+  handleShuffle() {
+    alert('handleShuffle')
+  }
+
   render() {
     return (
       <div>
-        <button>Shuffle</button>
+        <button onClick={this.handleShuffle}>Shuffle</button>
       </div>
     )
   }
 }
 
 class Options extends React.PureComponent {
+  handleRemoveAll() {
+    alert('handleRemove')
+  }
+
   render() {
     const { options } = this.props
 
     return (
-      <ol>
+      <div>
+        <button onClick={this.handleRemoveAll}>Remove all</button>
         {options.map((option) => {
           return <Option option={option} key={option} />
         })}
-      </ol>
+      </div>
     )
   }
 }
@@ -62,9 +93,11 @@ class Option extends React.PureComponent {
 
 class AddOption extends React.PureComponent {
   render() {
+    const { handleAddOption } = this.props
+
     return (
-      <form>
-        <input type="text" />
+      <form onSubmit={handleAddOption}>
+        <input type="text" name="option" />
         <button>Add Option</button>
       </form>
     )
